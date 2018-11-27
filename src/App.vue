55f9d51ea73548d443425fca7bcebbec2d97b5ab
rxliuli <rxliuli@gmail.com>
2018-11-27 18:48:08 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <normal-watch />
+        <br />
+        <deep-watch />
+        <br />
+        <deep-watch-for-var />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import NormalWatch from './components/NormalWatch'
+import DeepWatch from './components/DeepWatch'
+import DeepWatchForVar from './components/DeepWatchForVar'
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [
+        { icon: 'bubble_chart', title: 'Inspire' }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  },
+  components: {
+    NormalWatch,
+    DeepWatch,
+    DeepWatchForVar
+  }
+}
+</script>
